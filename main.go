4bd0a2f1a6393e15509562c5f75428aa13d457c4
Firdavs <firdavs792@gmail.com>
2022-03-12 14:08:17 +0500

    feat: add cli interface to handling blockchain data

--- main.go
+++ main.go
@@ -3,22 +3,42 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"os"
 	"strconv"
 	"strings"
 
 	"github.com/zfirdavs/go-blockchain/blockchain"
 )
 
-func main() {
-	flag.Parse()
+type CommandLine struct {
+	blockchain *blockchain.BlockChain
+}
 
-	blockChain := blockchain.InitBlockChain()
-	blockChain.AddBlock("the first block after Genesis")
-	blockChain.AddBlock("the second block after Genesis")
-	blockChain.AddBlock("the third block after Genesis")
+func (cli *CommandLine) printUsage() {
+	fmt.Println("Usage: ")
+	fmt.Println(" add -block BLOCK_DATA - add a block to the chain")
+	fmt.Println(" print - Prints the blocks in the chain")
+}
 
-	fmt.Println(strings.Repeat("-", 100))
-	for _, block := range blockChain.Blocks {
+func (cli *CommandLine) validateArgs() {
+	if len(os.Args) < 2 {
+		cli.printUsage()
+		return
+	}
+}
+
+func (cli *CommandLine) addBlock(data string) {
+	cli.blockchain.AddBlock(data)
+	fmt.Println("added block!")
+}
+
+func (cli *CommandLine) printChain() {
+	iter := cli.blockchain.Iterator()
+
+	for {
+		block := iter.Next()
+
 		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
 		fmt.Printf("Data in Block: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
@@ -27,5 +47,57 @@ func main() {
 		poW := blockchain.NewProof(block)
 		fmt.Printf("PoW validation: %s\n", strconv.FormatBool(poW.Validate()))
 		fmt.Println()
+
+		if len(block.PrevHash) == 0 {
+			break
+		}
 	}
+}
+
+func (cli *CommandLine) run() {
+	cli.validateArgs()
+
+	addBlockCmd := flag.NewFlagSet("add", flag.ExitOnError)
+	printChainCmd := flag.NewFlagSet("print", flag.ExitOnError)
+	addBlockData := addBlockCmd.String("block", "", "Block data")
+
+	var err error
+	switch os.Args[1] {
+	case "add":
+		err = addBlockCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Fatal(err)
+		}
+	case "print":
+		err = printChainCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Fatal(err)
+		}
+	default:
+		cli.printUsage()
+		return
+	}
+
+	if addBlockCmd.Parsed() {
+		if *addBlockData == "" {
+			addBlockCmd.Usage()
+			return
+		}
+		cli.addBlock(*addBlockData)
+	}
+
+	if printChainCmd.Parsed() {
+		cli.printChain()
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	blockChain := blockchain.InitBlockChain()
+	defer blockChain.Database.Close()
+
+	cli := CommandLine{blockChain}
+	cli.run()
+	fmt.Println(strings.Repeat("-", 100))
 }
