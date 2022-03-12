54261d605fdd142f44786de03373a2797745b1b7
Firdavs <firdavs792@gmail.com>
2022-03-12 14:04:54 +0500

    feat: add gob encoding and decoding blocks method

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,5 +1,11 @@
 package blockchain
 
+import (
+	"bytes"
+	"encoding/gob"
+	"log"
+)
+
 func Genesis() *Block {
 	return createBlock("Genesis", []byte{})
 }
@@ -7,6 +13,27 @@ func Genesis() *Block {
 type Block struct {
 	Hash, Data, PrevHash []byte
 	Nonce                int
+}
+
+func (b *Block) Serialize() []byte {
+	var res bytes.Buffer
+	encoder := gob.NewEncoder(&res)
+
+	if err := encoder.Encode(b); err != nil {
+		log.Fatal("failed to encode block ", err)
+	}
+
+	return res.Bytes()
+}
+
+func Deserialize(data []byte) *Block {
+	var block Block
+	decoder := gob.NewDecoder(bytes.NewReader(data))
+
+	if err := decoder.Decode(&block); err != nil {
+		log.Fatal("failed to decode block ", err)
+	}
+	return &block
 }
 
 func createBlock(data string, prevHash []byte) *Block {
